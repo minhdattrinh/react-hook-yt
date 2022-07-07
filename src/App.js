ea468a7b17e195b74edbae85c98009100eeadf85
minhdattrinh <minhdathht@gmail.com>
2022-07-07 15:25:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import "./App.scss";
+
+import ColorBox from "./components/ColorBox";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>;
+      <ColorBox />
     </div>
   );
 }
