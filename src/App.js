2a64e4802741e94b34cff1b7ce3423192b9fe010
minhdattrinh <minhdathht@gmail.com>
2022-07-07 21:59:38 +0700

    updatbai2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
 import "./App.scss";
 
 import ColorBox from "./components/ColorBox";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love you!" },
+    { id: 2, title: "I love you!hihi" },
+    { id: 3, title: "I love you!hehe" },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="App">
-      <h1>Welcome</h1>;
-      <ColorBox />
+      <h1>Welcome</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
--- src/components/ColorBox/index.jsx
+++ src/components/ColorBox/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 import "./ColorBox.scss";
 
 ColorBox.propTypes = {};
